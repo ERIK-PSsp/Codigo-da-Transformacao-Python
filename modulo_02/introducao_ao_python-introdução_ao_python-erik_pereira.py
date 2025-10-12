b9c47a3c8acc68e3ef5e85769b70bf5e51fbf49c
erik pereira em vocacao <erikpereirsilva@gmail.com>
2025-10-12 12:16:33 -0300

    Atividades 1 e 2, commit

---
+++ modulo_02/introducao_ao_python-introdução_ao_python-erik_pereira.py
@@ -0,0 +1,15 @@
+# esta é uma aplicação simples que interage com o usuário
+# ela pede que o usuário escolha uma bebida e depois exibe uma mensagem personalizada
+# print ('Bem-vindo, visitante'), função print exibe uma mensagem na tela
+# variável bebida_escolhida armazena a bebida escolhida pelo usuário
+# a função input exibe uma mensagem na tela e espera o usuário digitar algo e pressionar Enter
+# f-strings são usadas para formatar strings, permitindo incluir variáveis dentro de strings
+# strings são sequências de caracteres, usadas para representar texto
+
+print ('Bem-vindo, visitante')
+
+bebida_escolhida = input ('Escolha uma bebida.')
+
+print (f"Aqui está a sua {bebida_escolhida}.")
+
+print ('Volte sempre😊!')
